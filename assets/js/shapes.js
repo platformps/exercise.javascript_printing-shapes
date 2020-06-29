a58b9dc894ed31b864f1d9b2e1f7c5ef81faab52
David Frary <fraryd@gmail.com>
2020-06-29 16:31:27 -0400

    finished edits on 'index.html'

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -1,31 +1,56 @@
-function getLine(length) {
-    // TODO - write method definition here
+
+
+  function getLine(length) {
+
+let output = "";
+for (let i=0; i<length; i++){
+    output += "*";
+}
+return output;
 }
 
-
-
-function getBox(width, height) {
-    // TODO - write method definition here
+ function getBox(width, height) {
+    let output = "";
+    for (let i=0; i<length; i++){
+        output += "*";
+    }
+    return output;
 }
 
 
 
 function getBottomLeftTriangle(length) {
-    // TODO - write method definition here
+    let output = "";
+    for (let i=0; i<length; i++){
+        output += "*";
+    }
+    return output;
 }
 
 
 function getUpperLeftTriangle(length) {
-    // TODO - write method definition here
+    let output = "";
+    for (let i=0; i<length; i++){
+        output += "*";
+    }
+    return output;
 }
 
 
 
 function getPyramid(length) {
-    // TODO - write method definition here
+    let output = "";
+    for (let i=0; i<length; i++){
+        output += "*";
+    }
+    return output;
 }
 
 
 function getCheckerboard(width, height) {
-    // TODO - write method definition here
+    let output = "";
+    for (let i=0; i<length; i++){
+        output += "*";
+    }
+    return output;
 }
