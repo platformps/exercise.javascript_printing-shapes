459c8a702ae02fae2e520962e8377f905fba5912
suresh kandagatla <surikandagatla@gmail.com>
2020-06-29 16:56:55 -0400

    Finished getLine and getBox functions

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -1,13 +1,26 @@
 function getLine(length) {
     // TODO - write method definition here
+    var output = "";
+    for (var i=0; i<length; i++){
+        output+= "*";
+    }
+    return output;
 }
 
 
 
 function getBox(width, height) {
     // TODO - write method definition here
+    var boxRow = "";
+    var boxColumn = "";
+    for(var i=1; i <= height; i++) {
+        for (j=1; j<= width; j++) {
+           boxRow += "*";
+        } boxRow = boxRow +"\n";
+    } return boxRow + boxColumn;
+    
 }
-
+   
 
 
 function getBottomLeftTriangle(length) {
