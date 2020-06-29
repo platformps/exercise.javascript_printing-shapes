07c29a4cb46228e00894ce51bdefcb956130be85
Laura Godinez <issagodinez@Lauras-MacBook-Pro.local>
2020-06-29 16:55:29 -0400

    Edits made during class

--- assets/js/footer-functions.js
+++ assets/js/footer-functions.js
@@ -5,44 +5,38 @@ getUpperLeftTriangleTests();
 getPyramidTests();
 getCheckerboardTests();
 
-
-
-
 function getLineTests() {
-    testGetLine("*", 1);
-    testGetLine("**", 2);
-    testGetLine("***", 3);
+  testGetLine("*", 1);
+  testGetLine("**", 2);
+  testGetLine("***", 3);
 }
 
-
 function getBoxTests() {
-    testGetBox("*\n", 1, 1);
-    testGetBox("**\n**\n", 2, 2);
-    testGetBox("***\n***\n***\n***\n", 3, 4);
+  testGetBox("*\n", 1, 1);
+  testGetBox("**\n**\n", 2, 2);
+  testGetBox("***\n***\n***\n***\n", 3, 4);
 }
 
 function getBottomLeftTriangleTests() {
-    testGetBottomLeftTriangle("*", 1);
-    testGetBottomLeftTriangle("*\n**", 2);
-    testGetBottomLeftTriangle("*\n**\n**", 3);
+  testGetBottomLeftTriangle("*", 1);
+  testGetBottomLeftTriangle("*\n**", 2);
+  testGetBottomLeftTriangle("*\n**\n**", 3);
 }
 
 function getUpperLeftTriangleTests() {
-    testGetUpperLeftTriangle("*", 1);
-    testGetUpperLeftTriangle("**\n*", 2);
-    testGetUpperLeftTriangle("***\n**\n*", 3);
+  testGetUpperLeftTriangle("*", 1);
+  testGetUpperLeftTriangle("**\n*", 2);
+  testGetUpperLeftTriangle("***\n**\n*", 3);
 }
-
 
 function getPyramidTests() {
-    testGetPyramid("*", 1);
-    testGetPyramid(" * \n***", 2);
-    testGetPyramid("  *  \n *** \n*****", 3);
+  testGetPyramid("*", 1);
+  testGetPyramid(" * \n***", 2);
+  testGetPyramid("  *  \n *** \n*****", 3);
 }
 
-
 function getCheckerboardTests() {
-    testGetCheckerboard(" *\n* ", 2, 2);
-    testGetCheckerboard(" * \n* *\n * ", 3, 3);
-    testGetCheckerboard(" * \n* *\n * \n* *\n", 3, 4);
+  testGetCheckerboard(" *\n* ", 2, 2);
+  testGetCheckerboard(" * \n* *\n * ", 3, 3);
+  testGetCheckerboard(" * \n* *\n * \n* *\n", 3, 4);
 }
--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -1,31 +1,53 @@
 function getLine(length) {
-    // TODO - write method definition here
+  let output = "";
+  let iterationCounter = 0;
+  for (iterationCounter; iterationCounter < length; iterationCounter++) {
+    output += "*";
+  }
+  return output;
 }
-
-
 
 function getBox(width, height) {
-    // TODO - write method definition here
+  let output = "";
+  let iterationCounter;
+  let iterationCounter_2;
+
+  for (iterationCounter = 0; iterationCounter < height; iterationCounter++) {
+    for (
+      iterationCounter_2 = 0;
+      iterationCounter_2 < width;
+      iterationCounter_2++
+    ) {
+      output += "*";
+    }
+    output += "\n";
+  }
+  return output;
 }
-
-
 
 function getBottomLeftTriangle(length) {
-    // TODO - write method definition here
+  let iterationCounter;
+  let iterationCounter_2;
+  for (iterationCounter = 0; iterationCounter < length; iterationCounter++) {
+    for (
+      iterationCounter_2 = 0;
+      iterationCounter_2 < iterationCounter;
+      iterationCounter_2
+    ) {
+      output += "*";
+    }
+    output += "br/>";
+  }
 }
-
 
 function getUpperLeftTriangle(length) {
-    // TODO - write method definition here
+  // TODO - write method definition here
 }
-
-
 
 function getPyramid(length) {
-    // TODO - write method definition here
+  // TODO - write method definition here
 }
 
-
 function getCheckerboard(width, height) {
-    // TODO - write method definition here
+  // TODO - write method definition here
 }
