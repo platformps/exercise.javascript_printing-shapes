4116be708ec6ef5ea377e9cb652062b2a8cf2f7f
Ezra Vance <3zravance@gmail.com>
2020-06-29 17:08:33 -0400

    getBox tests passed.

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -12,6 +12,16 @@ function getLine(length) {
 
 function getBox(width, height) {
     // TODO - write method definition here
+    var answer = "";
+    var userIn1 = width;
+    var userIn2 = height;
+    for(let x = 1; x <= height; x++){
+        for(let y = 1; y <= width; y++){
+            answer += "*";
+        }
+        answer += "\n";
+    }
+    return answer;
 }
 
 
