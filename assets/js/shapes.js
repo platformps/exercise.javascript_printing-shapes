e1c9af9b3a30f56ad9a7a993dc9e0ac94d73982c
Ghassan Nasr <ghassan.nasr@gmail.com>
2020-06-29 19:20:13 -0400

    All functions passed the tests, some implementations are more elegant than others

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -25,30 +25,76 @@ function getBox(width, height) {
 
 function getBottomLeftTriangle(length) {
   // TODO - write method definition here
+  var str = "";
+  for (var i = 1; i <= length; i++) {
+    for (var j = 0; j < i; j++) {
+      str += "*";
+    }
+    if (i != length) {
+      str += "\n";
+    }
+  }
+
+  return str;
 }
+
 
 
 function getUpperLeftTriangle(length) {
   // TODO - write method definition here
-var str = "";
-for (var i = length; i > 0; i--) {
-  for (var k = 0; k < i; k++) {
-    str += "*";
+  var str = "";
+  for (var i = length; i > 0; i--) {
+    for (var k = 0; k < i; k++) {
+      str += "*";
+    }
+    if (i != 1) {
+      str += "\n";
+    }
   }
-  if(i != 1) {
-    str += "\n";
-  }
-}
-return str;
+  return str;
 }
 
 
 
 function getPyramid(length) {
   // TODO - write method definition here
+  var str = "";
+  for (var layer = 1; layer <= length; layer++) {
+
+    //add leading spaces
+    for (var j = 0; j < length - layer; j++) {
+      str += " ";
+    }
+    //add asterisks
+    for (var j = 0; j < 2 * layer - 1; j++) {
+      str += "*";
+    }
+    //addiing spaces
+    for (var j = 0; j < length - layer; j++) {
+      str += " ";
+    }
+    str += (layer<length ? "\n" : "");
+  }
+  return str;
 }
 
 
 function getCheckerboard(width, height) {
   // TODO - write method definition here
+  var str = "";
+
+  for (var layer = 1; layer <= height; layer++) {
+    var isOddLayer = (layer % 2) == 1;
+    for (var slot = 1; slot <= width; slot++) {
+      var isOddSlot = (slot % 2 == 1);
+      if (isOddLayer) {
+        str += isOddSlot ? " " : "*";
+      }
+      else {
+        str += isOddSlot ? "*" : " ";
+      }
+    }
+    str += "\n";
+  }
+  return str;
 }
