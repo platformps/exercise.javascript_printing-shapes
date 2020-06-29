b27edbd5e9c3b4bc416582ec34a20a2db2ab957e
David Frary <fraryd@gmail.com>
2020-06-29 16:53:44 -0400

    finished edits on 'index.html'

--- assets/js/header-functions.js
+++ assets/js/header-functions.js
@@ -1,47 +1,24 @@
-getLineTests();
-getBoxTests();
-getBottomLeftTriangleTests();
-getUpperLeftTriangleTests();
-getPyramidTests();
-getCheckerboardTests();
+function testGetLine(expectedOutput, numberOfStars) {
+    test(expectedOutput, getLine, numberOfStars);
+}
 
-
-
-
-function getLineTests() {
-    testGetLine("*", 1);
-    testGetLine("**", 2);
-    testGetLine("***", 3);
+function testGetBox(expectedOutput, width, height) {
+    test(expectedOutput, getBox, width, height);
 }
 
 
-function getBoxTests() {
-    testGetBox("*\n", 1, 1);
-    testGetBox("**\n**\n", 2, 2);
-    testGetBox("***\n***\n***\n***\n", 3, 4);
+function testGetCheckerboard(expectedOutput, width, height) {
+    test(expectedOutput, getCheckerboard, width, height);
 }
 
-function getBottomLeftTriangleTests() {
-    testGetBottomLeftTriangle("*", 1);
-    testGetBottomLeftTriangle("*\n**", 2);
-    testGetBottomLeftTriangle("*\n**\n***", 3);
+function testGetPyramid(expectedOutput, numberOfStars) {
+    test(expectedOutput, getPyramid, numberOfStars);
 }
 
-function getUpperLeftTriangleTests() {
-    testGetUpperLeftTriangle("*", 1);
-    testGetUpperLeftTriangle("**\n*", 2);
-    testGetUpperLeftTriangle("***\n**\n*", 3);
+function testGetUpperLeftTriangle(expectedOutput, numberOfStars) {
+    test(expectedOutput, getUpperLeftTriangle, numberOfStars);
 }
 
-
-function getPyramidTests() {
-    testGetPyramid("*", 1);
-    testGetPyramid(" * \n***", 2);
-    testGetPyramid("  *  \n *** \n*****", 3);
+function testGetBottomLeftTriangle(expectedOutput, numberOfStars) {
+    test(expectedOutput, getBottomLeftTriangle, numberOfStars);
 }
-
-
-function getCheckerboardTests() {
-    testGetCheckerboard(" *\n* \n", 2, 2);
-    testGetCheckerboard(" * \n* *\n * \n", 3, 3);
-    testGetCheckerboard(" * \n* *\n * \n* *\n", 3, 4);
