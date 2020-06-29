175c027f230e898723a2ff5321c9b027adc5e67f
Resilient08 <58645288+Resilient08@users.noreply.github.com>
2020-06-29 16:55:15 -0400

    Javascript Day 1 Work

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -1,12 +1,29 @@
 function getLine(length) {
     // TODO - write method definition here
+    let output = "";
+    let length = 7;
+    for (let i=0; i<7; i++) {
+        output +="*";
+    }
+
+    return output;
+     
 }
+
 
 
 
 function getBox(width, height) {
     // TODO - write method definition here
+    let output = "";
+    for ( let i=0; i<width; i++ && let i=0; i<height; i++) {
+        output +="*";
+    } 
+{
+
+    return output;
 }
+
 
 
 
