f049474372bff4117ad97c3e6319834c5a53c742
suresh kandagatla <surikandagatla@gmail.com>
2020-06-29 23:41:00 -0400

    Added getCheckerboard (not a perfect solution)

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -51,22 +51,37 @@ function getUpperLeftTriangle(length) {
 function getPyramid(length) {
     // TODO - write method definition here
     var output = "";
-    for(i=1;i <=length; i++){
-      for(k=1; k<=length-i; k++){
-        output+= " ";
-      }
-      for(j=1; j<=i;j++ ){
-        for(l=1; l<j; l++){
-           output+= "*";
-        }
-        output+= "*";
-        
-      }
-      output += "\n";
-    } 
+for(i=1;i <=length; i++){
+  for(k=1; k<=length-i; k++){
+    output+= " ";
+  }
+  for(j=1; j<=i;j++ ){
+    for(l=1; l<j; l++){
+       output+= "*";
+    }
+    output+= "*";
+    
+  }
+  output += "\n";
+} output += "\n";
+    
     return output;
 }
 
 function getCheckerboard(width, height) {
     // TODO - write method definition here
+ 
+var output = "";
+var check;
+for (i=0; i< height; i++){
+  if(i % 2){
+    check= "* ";
+  } else {
+    check= " *";
+  }
+  for(j=0; j< height/2; j++){
+    output+= check;
+  }
+   output+=  "\n";
+} return output;
 }
