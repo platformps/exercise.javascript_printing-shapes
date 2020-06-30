bff416d0dae2ab2913d318afb148fe75e4c47f49
Julia Waclawek <julie.waclawek@gmail.com>
2020-06-30 09:08:49 -0400

    first commit

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -1,28 +1,65 @@
-function getLine(length) {
+function getLine (length) {
     // TODO - write method definition here
+let output = "";
+    for(let i=0; i<7; i++){
+        output += "*";
+    }
+    return output;
 }
 
-
-
-function getBox(width, height) {
+function getBox( 7, 4) {
     // TODO - write method definition here
+    let output = " ";  
+    for(let i=0; i<7; i++)
+    {
+    for(let j=0; j<4; j++)
+    {
+    output += output + "*";
+    }
+    output = "<br/>";
+    return output;
 }
-
-
+ 
 
 function getBottomLeftTriangle(length) {
     // TODO - write method definition here
+let output = "";
+for(let i=1; i<=6; i++)
+{
+    for(let j=1; j<=i; j++){
+    output = output + "*";
+    }
+    output = "<br/>"
 }
-
+    return output;
+}
 
 function getUpperLeftTriangle(length) {
     // TODO - write method definition here
-}
-
+    let output = "";
+    for(let i=1; i<=5; i--)
+    {
+        for(let j=1; j<=i; j++){
+        output = output + "*";
+        }
+        output = "<br/>"
+    }   
+        return output;
+    }
 
 
 function getPyramid(length) {
     // TODO - write method definition here
+for (let i = 0; i < n; i++) {
+    let output = '';
+    for (let j = 1; j < n-i; j++) {
+      output = output + ' ';
+    }
+    for (let k = 1; k <= (2*i+1); k++) {
+      output = output + '*';
+    }
+    return output;
+  }
 }
 
 
