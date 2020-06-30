57c26444e542bf1b8a370cf1db1a84a42210bbdb
Laura Godinez <issagodinez@Lauras-MacBook-Pro.local>
2020-06-30 08:57:45 -0400

    edits

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -62,9 +62,48 @@ function getUpperLeftTriangle(length) {
 }
 
 function getPyramid(length) {
-  // TODO - write method definition here
+ let shape="";
+ let iterationCounter;
+ let iterationCounter_2;
+ let iteration_Counter_3;
+ let line;
+ if (length ==0 || length==1) return getLine(length);
+ for (iterationCounter=1; iterationCounter<=length;iterationCounter++){
+     line = "";
+     for (iterationCounter_2=1 ;iterationCounter_2<=length;iterationCounter_2++){
+         line +=" ";
+
+     }
+     for (iteration_Counter_3=1;iteration_Counter_3<=iterationCounter+2; iteration_Counter_3++{
+         line+="*";
+         shape+= line + "\n";
+     }
+    }
+     return shape;
 }
 
+
+
 function getCheckerboard(width, height) {
-  // TODO - write method definition here
+    let output="";
+
+    let iterationCounter;
+    let iterationCounter_2;
+    let iteration_Counter_3=1;
+    
+    for( iterationCounter = 1; iterationCounter <= height; iterationCounter++) {
+        for(iterationCounter_2 = width; iterationCounter_2 > 0; iterationCounter_2--) {
+            if(iteration_Counter_3 % 2 == 0) {
+                output += "*"; 
+            }
+            else {
+                output += " "; 
+            }
+            iteration_Counter_3++; 
+        }
+        if(width % 2 == 0) { iteration_Counter_3++; } 
+        output += "\n";
+    }
+    return output;
+
 }
