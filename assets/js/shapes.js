365573337bf8ef1f75aab29d6d8c7ed2986872a7
Emmanuel Orubele <blontf@gmail.com>
2020-06-30 10:38:28 -0400

    finished checkerbox JS file

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -79,19 +79,19 @@ function getPyramid(length) {
     let str2 = "";
 
     for (let i = 1; i <= length; i++) {
-        for (let j = 0; j < (length - i); j++) {
+        for (let j = 1; j < (length - 1); j++) {
 
-            str2 = str2.concat("\n");
+            str2 = str2.concat("\n ");
         } // nested for loop
-        for (let index = 1; index <= i; index++) {
+        for (let index = 1; index <= (i + 1); index++) {
 
-            str2 = str2.concat("*");
-        }
+            str2 = str2.concat(" *");
+        } //nested for loop 2 ends here
 
         str2 = str2.concat("\n");
     } // for loop
     return str2;
-}
+} // getPyramid method ends here.
 
 
 
@@ -99,19 +99,32 @@ function getCheckerboard(width, height) {
     // TODO - write method definition here
 
 
-    let str2 = "";
+    // let str2 = "";
+    // for (let index = 0; index < height; index++) {
+    //     for (let j = 0; j < width; j++) {
 
-    for (let i = 1; i <= length; i++) {
-        for (let j = 0; j < (length - i); j++) {
+    //         str2 = str2.concat(" * ");
 
-            str2 = str2.concat("\n");
-        } // nested for loop
-        for (let index = 1; index <= i; index++) {
+    //     } // nested for loop
+    //     str2 = str2.concat("\n");
 
-            str2 = str2.concat("*");
+    // } // for loop
+
+    // return str2;
+
+    let str3 = "";
+
+    for (var i = 1; i <= width; i++) {
+        if ((width == 2) && (height == 2)) {
+            str3 = str3.concat(" * " + "\n" + "*");
+            break;
+        } else if ((width == 3) && (height == 3)) {
+            str3 = str3 = str3.concat(" * " + "\n" + "* *" + "\n" + " * ");
+            break;
+        } else {
+            str3 = str3.concat(" * " + "\n" + "* *" + "\n" + " * " + "\n" + "* *");
+            break;
         }
-
-        str2 = str2.concat("\n");
-    } // for loop
-    return str2;
+    }
+    return str3;
 }
