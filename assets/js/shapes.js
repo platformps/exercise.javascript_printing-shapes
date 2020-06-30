e90cb901099b6c2fb54979ac0c3f3489bfa13cb5
monicadeshmukh <renavikar_monica@yahoo.com>
2020-06-29 20:25:11 -0400

    checkerboard

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -113,6 +113,40 @@ function getCheckerboard(width, height) {
     // TODO - write method definition here
     var output = "";
     /* print width no. of stars in two lines. print a total of height no. of lines. */
-   output = parseInt(11/2);
+   var line1 = parseInt(width/2);
+   var line2 = line1;
+   if (width%2 !=0) /*odd number*/
+    line2++;
+    var outputL1 = " ";
+    var outputL2 = "";
+
+    for (var l1 = line1; l1 > 0; --l1){
+        if( l1 !=1)
+         outputL1 += "* ";
+         else
+         outputL1 += "*";
+    }
+    for (var l2 = line2; l2 > 0; l2--){
+        if (l2 !=1)
+            outputL2 += "* ";
+        else
+            outputL2 += "*";
+    }
+    while(height > 0)
+    {
+        output += outputL1;
+        if (height != 1)
+            output += "\n";
+        else
+            break;
+        height--;
+        output += outputL2;
+        if (height != 1)
+            output += "\n";
+        else
+            break;
+        height--;
+    }
+     //return "Line1: "+ line1 + " " + "Line2: "+ line2 + " " + "outputL1: "+ outputL1 + " " + "outputL2: "+ outputL2 + "output: " + output ;
      return output;
 }
