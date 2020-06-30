8c0e778ceefc80ec0c15e07e9d7ae0fcf79bb9be
unknown <dyirchott@hotmail.com>
2020-06-29 23:56:49 -0400

    All but one done. Need to clean up code

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -7,11 +7,10 @@ function getLine(length) {
 }
 
 
-
 function getBox(width, height) {
     // TODO - write method definition here
     var stars = "";
-    for(rows = 0; rows < height; rows++) {       
+    for(rows = 0; rows < height; rows++) {     
         for (columns = 0; columns < width; columns++) {
             stars += "*";
         }  
@@ -21,11 +20,10 @@ function getBox(width, height) {
 }
 
 
-
 function getBottomLeftTriangle(length) {
     // TODO - write method definition here
     var stars = "";
-    for(i = 1; i <= length; i++) {       
+    for(i = 1; i <= length; i++) {    
         for (j = 0; j < i; j++) {
             stars += "*";
         }  
@@ -40,24 +38,67 @@ function getBottomLeftTriangle(length) {
 function getUpperLeftTriangle(length) {
     // TODO - write method definition here
     var stars = "";
-    for(i = 1; i <= length; i++) {       
-        for (j = length; j < i; j--) {
+    for(i = length; i >= 1; i--) { 
+        if (i!=length) {
+        stars += "\n";    
+        }  
+        for (j = 1; j <= i ; j++) {
             stars += "*";
+        }              
+    }
+    return stars;
+}
+
+
+function getPyramid(length) {
+    // TODO - write method definition here
+    var stars = "";
+    var spacing = length;
+    var newPyramid = true;
+    for (i = 1; i <= length; i++) {   
+
+        for (j = 0; j < spacing -1; j++) {
+            stars += " ";
+        }
+
+        for (k = 1; k <= i; k++) {
+            if (i == 1 && k==1 && newPyramid) {
+                stars += "*";
+                spacing--;
+                
+            } else {
+                stars += "**";
+                spacing--;
+            }
         }  
+
+    
+
         if (i != length) {
+            if (!newPyramid) {
+                stars=stars.substring(0, stars.length - 1);
+            }
             stars += "\n"; 
+            newPyramid = false;
         }  
+    }
+    if (stars.indexOf("*")) {
+        stars=stars.substring(0, stars.length - 1);
     }
     return stars;
 }
 
 
 
-function getPyramid(length) {
-    // TODO - write method definition here
-}
 
 
 function getCheckerboard(width, height) {
     // TODO - write method definition here
+
 }
+
+
+
+
+
+
