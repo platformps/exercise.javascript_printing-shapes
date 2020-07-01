bd61e8e88db811a3d8662fcb74c180723f3f1b19
unknown <dyirchott@hotmail.com>
2020-07-01 19:06:03 -0400

    finished shapes

--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -89,6 +89,30 @@ function getPyramid(length) {
 function getCheckerboard(width, height) {
     // TODO - write method definition here
     var stars = "";
+    var pyrHeight;
+    for (pyrHeight = 1; pyrHeight <= height; pyrHeight++) {
+        if (width < 3){
+            if (pyrHeight % 2 != 0) {
+                stars += " *";
+            } else {
+                 stars += "* ";
+            }
+        }
+        if (width >= 3) {
+            if (pyrHeight % 2 != 0) {
+                stars += " * ";
+            } else {
+                stars += "* *";
+            }
+        }
+    stars +="\n";
+    }
+    return stars;
+}
+   
+   /* ===
+
+    var stars = "";
     var counter3 = 0;
     var counter1_2;
     for(rows = 0; rows < height-1; rows++) {     
@@ -101,57 +125,17 @@ function getCheckerboard(width, height) {
                 if (counter1_2 % 2 == 0) {
                 stars += "*\n * \n"
                 } else {
-                    stars +=" *\n* "
+                    if (height < 3) {
+                        stars +=" *\n* "
+                    } else {
+                        stars += " * \n*"
+                    }
                 }
 
             }
         }  
         stars += "\n";      
-    }
-    return stars;
-}
-
-
-
-
-/*
-    var stars = "";
-    var spacing = length;
-    var diamondLevel = 0;
-    for (i = 1; i <= length; i++) {   
-        for (j = 0; j < spacing -1; j++) {
-            stars += " ";
-        }
-        for (k = 1; k <= i; k++) {
-            if (i == 1 && k==1 && newPyramid) {
-                stars += "*";
-                spacing--;   
-            } else {
-                stars += "**";
-                spacing--;
-            }
-        }  
-        if (i != length) {
-            if (!newPyramid) {
-                stars=stars.substring(0, stars.length - 1);
-            }
-            for (j = 0; j < length -1; j++) {
-                stars += " ";
-            
-            stars += "\n"; 
-            newPyramid = false;
-            }
-        }  
-    }
-    if (stars.indexOf("*")) {
-        stars=stars.substring(0, stars.length - 1);
-    }
-    return stars;
-}
-
-
-*/
-
-
-
+    }    */
+//    return stars;
+//}
 
